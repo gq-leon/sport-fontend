8734686c7974041150380d21a8bcbf6582bcec33
Gao <gaoq.leon@gmail.com>
2024-12-18 16:04:03 +0800

    fix: 当增加输出内容时，才不会报错
    问题需要排查

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo 'start run entrypoint script'
+
 # 替换环境变量
 envsubst '${API_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+echo 'end run entrypoint script'
 
 # 执行 CMD
 exec "$@" 
