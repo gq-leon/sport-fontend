51547817a11836d6ea9204e1ce5f22fd19f2a309
Gao <gaoq.leon@gmail.com>
2024-12-18 14:43:23 +0800

    feat: 增加CSP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
 
 const app = createApp(App)
+
+// 设置 CSP 头
+app.config.globalProperties.$http = axios.create({
+    headers: {
+        'Content-Security-Policy': "upgrade-insecure-requests; default-src 'self' http:;"
+    }
+})
 
 app.use(router)
 
